7847caa8c6a98172af41affdbb7960e6a21ef44a
enricca <enric.cosp.arque@gmail.com>
2024-08-03 13:44:42 +0200

    Update dl6_lobulation_nodules.py

--- src/Segmentation/00_final_execution/train/dl6_lobulation_nodules.py
+++ src/Segmentation/00_final_execution/train/dl6_lobulation_nodules.py
@@ -1,16 +1,12 @@
 import sys
 import getpass
-# if getpass.getuser() == 'manuel':
-sys.path.append('/home/manuel/lung_cancer_isbi18/src')
 import os
 import random
 import logging
 import argparse
-
 import numpy as np
 import pandas as pd
 from dl_model_patches import  common
-
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
